dcc24871cc456b8da492fcc211b62c44c7791947
boilfish <szywings@qq.com>
2023-03-24 00:49:29 +0800

    tflite branch create

--- model_disassemble_pytorch/exc_test.py
+++
@@ -1,8 +0,0 @@
-import json
-import torch
-import os
-os.chdir(os.path.dirname(__file__))
-PICTURE_DIR = "./src/pictures/"
-INDEX_DIR = "./src/"
-MODEL_SAVE_DIR = "./src/models/"
-CONFIG_DIR = "./src/"
