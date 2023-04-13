1d24ec8f254a0d105cad5840d22d02e2e7f6e6ae
boilfish <szywings@qq.com>
2023-04-13 21:39:08 +0800

    script modify

--- model_disassemble_pytorch/model_disassemble.py
+++ model_disassemble_pytorch/model_disassemble.py
@@ -18,7 +18,7 @@ input_shape = (3, 224, 224)  # 输入数据,改成自己的输入shape
 input = torch.randn(batch_size, *input_shape)   # 生成张量
 
 class Predictor():
-    def __init__(self,src_model,transformer) -> None:
+    def __init__(self,src_model,transformer,Primary_Data_DIR) -> None:
         self.flatten = False
         self.constr_info = {}
         self.layer_container = []
@@ -29,7 +29,7 @@ class Predictor():
         with open(INDEX_DIR+'imagenet_class_index.json') as labels_file:
             self.labels = json.load(labels_file)
         self.disassemble_mode = True
-
+        self.PrimaryData=self.test_load(Primary_Data_DIR)
     def __str__(self) -> str:
         print("Printing model config:")
         print(self.constr_info)
@@ -37,11 +37,10 @@ class Predictor():
         print(self.layer_container)
 
     def test_load(self, dir):
-        testpng = read_image(dir)
-        testpng = self.transformer(testpng)
-        testpng = testpng.unsqueeze(0)
-        self.testpng = testpng
-        self.testinput=copy.deepcopy(testpng)
+        pridata = read_image(dir)
+        pridata = self.transformer(pridata)
+        pridata = pridata.unsqueeze(0)
+        return pridata
     def img2tensor_save(self):
         pass
 
@@ -74,7 +73,7 @@ class Predictor():
 
     def dump_disassembled_file(self):
         counter = 0
-        x=copy.deepcopy(self.testpng)
+        x=copy.deepcopy(self.PrimaryData)
         for model in self.layer_container:
             model.eval()
             shape=list(x.shape)
@@ -104,35 +103,38 @@ class Predictor():
                 model_dir+"testmodel_"+str(i)+".pth"))
         pass
     
-    def testonnx_func(self, config_dir, model_dir):
-        
+    def testonnx_func(self, config_dir, model_dir,data_dir):
+        #Loading Configuration
         with open(config_dir+"configure.json") as f_1:
             self.constr_info = json.load(f_1)
         for i in range(self.constr_info['lenth']):
             self.layer_container.append(torch.load(
                 model_dir+"testmodel_"+str(i)+".pth"))
-        x2=np.array(self.testpng)
-        x = self.testpng
-        x3=copy.deepcopy(self.testpng)
-        counter=0
-        for i in range(self.constr_info['lenth']):
-            resnet_session = rt.InferenceSession(model_dir+"testmodel_"+str(i)+".onnx")
-            input_name = resnet_session.get_inputs()[0].name
-            x2 = resnet_session.run([],{input_name:x2})[0]
-            counter+=1
-            self.layer_container[i].eval()
-            x = self.layer_container[i](x)
-        x = x.argmax(dim=1)
-        x2=np.argmax(x2)
-        x3=self.src_model(x3)
-        x3=x3.argmax(dim=1)
-        if(x.item()==x2.item()):
-            if(x2.item()==x3.item()):
-                print(f"Prediction res: {self.labels[str(x.item())]}")
+        for i in range(1,100):
+        #Input
+            self.PrimaryData=self.test_load(data_dir+str(i)+".jpg")
+            x2=np.array(self.PrimaryData)
+            x = self.PrimaryData
+            x3=copy.deepcopy(self.PrimaryData)
+            counter=0
+            for i in range(self.constr_info['lenth']):
+                resnet_session = rt.InferenceSession(model_dir+"testmodel_"+str(i)+".onnx")
+                input_name = resnet_session.get_inputs()[0].name
+                x2 = resnet_session.run([],{input_name:x2})[0]
+                counter+=1
+                self.layer_container[i].eval()
+                x = self.layer_container[i](x)
+            x = x.argmax(dim=1)
+            x2=np.argmax(x2)
+            x3=self.src_model(x3)
+            x3=x3.argmax(dim=1)
+            if(x.item()==x2.item()):
+                if(x2.item()==x3.item()):
+                    print(f"Prediction res: {self.labels[str(x.item())]}")
         pass
 
     def predict(self):
-        x = self.testpng
+        x = self.PrimaryData
         for model in self.layer_container:
             # print(model) #output model
             model.eval()
@@ -142,20 +144,17 @@ class Predictor():
 
 
 if __name__ == "__main__":
-    resnet34 = model.resnet34(pretrained=True)
+    #resnet34 = model.resnet34(pretrained=True)
     weights = ResNet18_Weights.DEFAULT.transforms()
-    src_model = model.resnet18(pretrained=True)
-    new_instance = Predictor(src_model=src_model,transformer=weights)
-    
+    src_model=model.resnet18(pretrained=True)
+    #src_model = torch.load("./src/src_models/resnet18-5c106cde.pth")
+    new_instance = Predictor(src_model=src_model,transformer=weights,Primary_Data_DIR=PICTURE_DIR+str(1)+".jpg")
     #new_instance.disassemble(flatten=True)
     #new_instance.dump_disassembled_file()
     #res = new_instance.predict()
     #print(res)
     new_instance.src_model.eval()
-    for i in range(1,100):
-        print(i)
-        new_instance.test_load(PICTURE_DIR + str(i)+".jpg")
-        new_instance.testonnx_func(CONFIG_DIR,MODEL_SAVE_DIR)
+    new_instance.testonnx_func(CONFIG_DIR,MODEL_SAVE_DIR,PICTURE_DIR)
     # new_instance.load_disassembled_file(CONFIG_DIR,MODEL_SAVE_DIR)
     
     
