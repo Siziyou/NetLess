4fdd5faf6f1afe7ee00533082442046faad45346
Siziyou <szywings@qq.com>
2023-05-01 18:29:34 +0800

    auto commit

--- demo/worker_func/func2/handler.py
+++ demo/worker_func/func2/handler.py
@@ -3,23 +3,45 @@ import numpy as np
 import orjson
 import os
 os.chdir(os.path.dirname(__file__))
-def handle(req):
-    body = orjson.loads(req)
-    if(body['s']==0):
-        return req
-    if(body['s']==2):
+
+
+class worker():
+    def __init__(self, req) -> None:
+        self.input = orjson.loads(req)
+        self.req = req
+        self.signal = input['s']
+        self.mapper = {
+            0: self.warmup(),
+            1: self.inference(),
+            2: self.classify()
+        }
+        pass
+
+    def process(self):
+        return self.mapper[self.signal]
+
+    def warmup(self):
+        return self.req
+
+    def inference(self):
+        session = rt.InferenceSession("model.onnx")
+        input_name = session.get_inputs()[0].name
+        x = np.array(self.input['d'], dtype=np.float32)
+        x = session.run([], {input_name: x})[0]
+        return orjson.dumps({"s": 1, "d": x}, option=orjson.OPT_SERIALIZE_NUMPY)
+
+    def classify(self):
         with open('labels.json', "rb") as labels_file:
             labels = orjson.loads(labels_file.read())
-        resnet_session = rt.InferenceSession("model.onnx")
-        input_name = resnet_session.get_inputs()[0].name
-        x = np.array(body['d'],dtype=np.float32)
-        x = resnet_session.run([], {input_name: x})[0]
+        session = rt.InferenceSession("model.onnx")
+        input_name = session.get_inputs()[0].name
+        x = np.array(self.input['d'], dtype=np.float32)
+        x = session.run([], {input_name: x})[0]
         x = np.argmax(x)
-        classify_res=labels[str(x.item())]
-        return orjson.dumps({"s":1,"d":classify_res})
-    if(body['s']==1):
-        resnet_session = rt.InferenceSession("model.onnx")
-        input_name = resnet_session.get_inputs()[0].name
-        x = np.array(body['d'],dtype=np.float32)
-        x = resnet_session.run([], {input_name: x})[0]
-        return orjson.dumps({"s":1,"d":x},option=orjson.OPT_SERIALIZE_NUMPY)
+        classify_res = labels[str(x.item())]
+        return orjson.dumps({"s": 1, "d": classify_res})
+
+
+def handle(req):
+    instance=worker(req)
+    return instance.process()
