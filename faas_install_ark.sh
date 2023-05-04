a96d078fcdb63b0520edb4f92e06296763bc7759
Siziyou <szywings@qq.com>
2023-05-04 21:41:33 +0800

    update dir

--- faas_install_ark.sh
+++
@@ -1,6 +0,0 @@
-curl -sLS https://get.arkade.dev | sudo sh
-arkade get faas-cli
-arkade install mongodb
-arkade install openfaas
-echo export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace default mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode) >> ~/.bashrc
-faas-cli secret create mongo-db-password --from-literal $MONGODB_ROOT_PASSWORD
